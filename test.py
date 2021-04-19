6d1eb0856467e65cd92d71e15cf8311d34933e5d
Mohit Kulkarni <mohitmk@iitk.ac.in>
2021-04-19 20:56:07 +0530

    test

--- test.py
+++ test.py
@@ -9,3 +9,13 @@ def target_f():
     
 
 
+ts_train, ts_test = np.arange(0, t_max, network.dt), np.arange(t_max, 2*t_max, network.dt)
+lw_f, lw_z = 3, 1.5
+
+# TRAIN Phase
+f_train = network.f(ts_train)
+
+fig = plt.figure(figsize=(15, 3*4))
+j = 1
+
+for i, t in enumerate(ts_train):
